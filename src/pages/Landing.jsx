d9a86db5d9a87230af9ed94a63f7b598822c4676
ruqayaalsammak <ruqayaalsammak2017@gmail.com>
2026-08-31 10:11:52 +0300

    Update the  Landing with the packages

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,25 @@
 const Landing = () => {
     return (
         <main>
-
         <section className="card">
             <h1>Welcome!</h1>
             <p>Sign up or sign in.</p>
-
         </section>
+
         <section className="card">
             <h1>Packages</h1>
-            
+            <div>
+                <h3>Bronze</h3>
+                <p>Essential event coordination and basic scheduling tools</p>
+            </div>
+            <div>
+                <h3>Silver</h3>
+                <p>Standard planning features with custom task lists.</p>
+            </div>
+            <div>
+                <h3>Gold</h3>
+                <p>Full-suite event management, timeline tracking, and priority support. </p>
+            </div>
         </section>
         </main>
     )
